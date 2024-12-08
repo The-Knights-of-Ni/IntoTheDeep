8e03eded6e2fa8f5d1413ad0bab041d30ad6830c
Rachael Lin <ralin23208@gmail.com>
2024-12-08 15:08:58 -0800

    Main teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testop/TeleOp2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testop/TeleOp2024.java
@@ -8,7 +8,11 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.Servo.Direction;
 
 @TeleOp
-public class TeleOpSliderWithEncoder extends LinearOpMode {
+public class TeleOp2024 extends LinearOpMode {
+    DcMotor frontLeftMotor;
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
     DcMotor sl;         // slider left
     DcMotor sr;         // slider right
     double motorTicks = 537.7;    // 312 RPM - SKU 5202-0002-0071.  145.1 for 1150 RPM
@@ -18,8 +22,8 @@ public class TeleOpSliderWithEncoder extends LinearOpMode {
 
     public enum Bucket {
         BOTTOM(0.0, 0.0),
-        LOW(2.5, -2.5),
-        HIGH(4.5, -4.5);
+        LOW(2.41, -2.5),    //LOW(2.41, -2.5). ==> 26" to the top
+        HIGH(5.31, -5.5);   //HIGH(5.31, -5.5) ==> 39" to the top
 
         private double leftTurnage;
         private double rightTurnage;
@@ -41,11 +45,15 @@ public class TeleOpSliderWithEncoder extends LinearOpMode {
 
     public enum Pivot {
         FULL(355.0),
-        ZERO(0.0),
         ONE(1.0),
-        SMALL(0.2),  // of 355 degree
+        SMALL(0.6),
         MEDIUM(0.4),
-        LARGE(0.85);
+        ZERO(0.0),      // Initial position 0
+        ZERO1(0.06),    // Intemediate position to slow down when putting back to ZERO
+        LARGE(0.85),    // Position to pick up sample
+        LARGE1(0.60),   // Intermediate position to slow down when putting to LARGE
+        CARRY(0.5);     // Position for carrying to bucket
+
 
         private double swing;
 
@@ -61,124 +69,130 @@ public class TeleOpSliderWithEncoder extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // Get motors
+        // Motors for wheels
+        frontLeftMotor = hardwareMap.dcMotor.get("fl");
+        backLeftMotor = hardwareMap.dcMotor.get("rl");
+        frontRightMotor = hardwareMap.dcMotor.get("fr");
+        backRightMotor = hardwareMap.dcMotor.get("rr");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Motots for sliders
         sl = hardwareMap.dcMotor.get("sl");
         sr = hardwareMap.dcMotor.get("sr");
 
-        // Connect to servos for arm/pivot
+        // Servos for arm/pivot
         al = hardwareMap.get(Servo.class, "al");
         ar = hardwareMap.get(Servo.class, "ar");
         c = hardwareMap.get(Servo.class, "c");
 
-        // Set motor's direction for left and right slider
+        // Set slider motor's direction for left and right slider
         sl.setDirection(DcMotorSimple.Direction.REVERSE);
         sr.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //Boolean checker variables to determine moving logic.
-        boolean buttona = true;
-        boolean buttonb = true;
-        /*
-        Work In Progress
-        boolean returnfromlow = false;
-        boolean returnfromhigh = true;
-        */
 
         waitForStart();
 
         if (isStopRequested()) return;
-        sl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        sr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        sl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        sr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         while (opModeIsActive()) {
-            // TODO: Add some tolerance
+            moveRobot();
+
+            // Manually move up and down for both sliders
             if (gamepad1.left_stick_y != 0) {
-                double slValue = -gamepad1.left_stick_y;
-                double srValue = -gamepad1.left_stick_y;
+                sl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                sr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                double slValue = -gamepad1.left_stick_y * 0.574; // this motor moves faster and needs to sync with sr
+                double srValue = -gamepad1.left_stick_y * -0.7; // 0.41, 0.5
 
                 sl.setPower(slValue);
+                sl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
                 sr.setPower(srValue);
+                sr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             }
 
             telemetry.addData("start: ", "encoder test");
             telemetry.update();
 
+            // Move up both sliders to HIGH position for top bucket
             if (gamepad1.y) {
                 moveSlidersWithEncoder(sl, sr, Bucket.HIGH);
             }
+
+            // Move up both sliders to LOW position for bottom bucket
             if (gamepad1.x) {
                 moveSlidersWithEncoder(sl, sr, Bucket.LOW);
             }
+
+            // Put back arm/pivot to initial position
             if (gamepad1.a) {
-                resetMotor();
-            }
-            if (gamepad1.b) {   // pivot & claw
-                // al.setDirection(Servo.Direction.REVERSE);
-                // ar.setDirection(Servo.Direction.REVERSE);
+                // resetMotor();
+                setPosition(al, ar, Pivot.ZERO1);
                 printCurrentPositionAndDirection(al, ar);
-
-                wait(2000);
-                setPosition(al, ar, Pivot.ZERO);
-                printCurrentPositionAndDirection(al, ar);
-
-                // wait(3000);
-                // setPosition(al, ar, small);
+                // wait(1000);
+                // setPosition(al, ar, Pivot.ZERO);
                 // printCurrentPositionAndDirection(al, ar);
+            }
 
-                wait(2000);
-                setPosition(al, ar, Pivot.LARGE);
-                printCurrentPositionAndDirection(al, ar);
-                wait(2000);
-
-                wait(3000);
-                setPosition(al, ar, Pivot.SMALL);
-                printCurrentPositionAndDirection(al, ar);
-
-                wait(3000);
-                setPosition(al, ar, Pivot.ZERO);
-                printCurrentPositionAndDirection(al, ar);
-
+            // Open claw
+            if (gamepad1.right_trigger != 0.0) {
                 clawOpen(c);
-                wait(2000);
+            }
+
+            // Position to pick up from submersible
+            if (gamepad2.right_trigger != 0.0) {
+                setPosition(al, ar, Pivot.LARGE1);
+            }
+
+            // Close clas
+            if (gamepad1.right_bumper) {
                 clawClose(c);
             }
 
-//             if (gamepad1.a && buttonb) {
-//                 runWithEncoder(2.5);          // 1 = 1 rotation, lower bucket = 2.5 turnage, go to 3 if need to go above bucket
-//                 buttona = false;
-//                 //returnfromlow = true;
-// //                runWithEncoder(0.5);
-//             }
-//             else if (gamepad1.a && buttona) {
-//                 runWithEncoder(-4);
-//                 buttona = true;
-//             }
+            // Drop down arm for picking up Sample
+            if (gamepad1.left_trigger != 0.0) {
+                setPosition(al, ar, Pivot.LARGE);
+            }
 
-//             if (gamepad1.b && buttona) {
-//                 runWithEncoder(6.5);          // 1 = 1 rotation, higher bucket = 6.5 turnage, go to 7 if need to go above bucket
-//                 buttonb = false;
-// //                runWithEncoder(0.5);
-//             }
-//             else if (gamepad1.b && buttonb){
-//                 runWithEncoder(4);
-//                 buttonb = true;
-//                 //returnfromhigh = true;
-//             }
-                /*
-                Work In Progress
-                if (gamepad1.x && returnfromlow) {
-                    runWithEncoder(-2.5);
-                }
-                else if (gamepad1.x && returnfromhigh){
-                    runWithEncoder(-6.5);
-                }
-                else{
-                    resetMotor();
-                }
-                */
+            // Move arm to scoring position for buckets
+            if (gamepad1.left_bumper) {
+                setPosition(al, ar, Pivot.CARRY);
+            }
+
+            // Move arm from initial position to pickup position
+            if (gamepad1.b) {
+                setPosition(al, ar, Pivot.ZERO);
+                printCurrentPositionAndDirection(al, ar);
+
+                setPosition(al, ar, Pivot.LARGE1);
+                printCurrentPositionAndDirection(al, ar);
+                wait(1000);
+                setPosition(al, ar, Pivot.LARGE);
+                printCurrentPositionAndDirection(al, ar);
+            }
         }
+    }
+
+    public void moveRobot() {
+        double y = -gamepad2.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad2.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad2.right_stick_x;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
     }
 
     public void moveSlidersWithEncoder(DcMotor sl, DcMotor sr, Bucket b) {
@@ -208,7 +222,7 @@ public class TeleOpSliderWithEncoder extends LinearOpMode {
         System.out.println("Left Current Position (before): " + sl.getCurrentPosition());
         System.out.println("Right Current Position (before): " + sr.getCurrentPosition());
 
-        sl.setPower(0.445);
+        sl.setPower(0.5);
         sr.setPower(0.5);
 
         sl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -234,15 +248,15 @@ public class TeleOpSliderWithEncoder extends LinearOpMode {
 
     public void clawOpen(Servo c){
         printClawPosition(c);
-        wait(1000);
-        c.setPosition(0.0);
+        // wait(1000);
+        c.setPosition(0.3);     //0.3=160degree
         printClawPosition(c);
     }
 
     public void clawClose(Servo c){
         printClawPosition(c);
-        wait(1000);
-        c.setPosition(0.19);
+        // wait(1000);
+        c.setPosition(0.09);     //0.09=.5" gap
         printClawPosition(c);
     }
 
